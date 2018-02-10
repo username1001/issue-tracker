bc71e68e19943ca4c2b73a25a01a6310c2e511b2
Matt <faymatt96@gmail.com>
2018-02-10 01:27:59 -0500

    Add react-router-bootstrap/update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,49 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: {
-    app: "./src/App.jsx",
+    app: './src/App.jsx',
     vendor: [
-      "react",
-      "react-dom",
-      "whatwg-fetch",
-      "babel-polyfill",
-      "react-router"
+      'react',
+      'react-dom',
+      'whatwg-fetch',
+      'babel-polyfill',
+      'react-router',
+      'react-bootstrap',
+      'react-router-bootstrap'
     ]
   },
   output: {
-    path: path.join(__dirname, "./static"),
-    filename: "[name].bundle.js"
+    path: path.join(__dirname, './static'),
+    filename: '[name].bundle.js'
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      filename: "vendor.bundle.js"
+      name: 'vendor',
+      filename: 'vendor.bundle.js'
     })
   ],
   module: {
     rules: [
       {
         test: /\.jsx$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015"]
+          presets: ['react', 'es2015']
         }
       }
     ]
   },
   devServer: {
     port: 8000,
-    contentBase: "static",
+    contentBase: 'static',
     proxy: {
-      "/api/*": {
-        target: "http://localhost:3000"
+      '/api/*': {
+        target: 'http://localhost:3000'
       },
       historyApiFallback: true
     }
   },
-  devtool: "source-map"
+  devtool: 'source-map'
 };
